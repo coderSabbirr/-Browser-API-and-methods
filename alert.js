12f8c072014bd3d8a4f3b87273a8f25c22b69607
Nahid Hasan Sabbir <sabbirnahid5817@gmail.com>
2021-09-04 23:13:00 +0600

    file add

---
+++ alert.js
@@ -0,0 +1,15 @@
+const alertBtn = () => {
+    alert('tiger is coming')
+};
+
+
+const confirmTour = () => {
+    const response = confirm(' are you going to picnic')
+    if(response === true) {
+        alert('taka patha bkash a')
+        console.log('true');
+    }
+     else{
+console.log('dhora ja biyadob');
+    }
+}
